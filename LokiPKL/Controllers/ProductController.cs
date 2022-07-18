b6cd72e13388ee3825a9acbb253aee2052432b5a
imloki2k <im.loki2k@gmail.com>
2022-07-18 18:49:49 +0700

    done layout user

---
+++ LokiPKL/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using LokiPKL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace LokiPKL.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Loki_PKLContext _context;
+
+        public ProductController(Loki_PKLContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details (int id)
+        {
+            var product = _context.Products
+                .Include(x => x.Category)
+                .Include(x => x.Brand)
+                .FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
