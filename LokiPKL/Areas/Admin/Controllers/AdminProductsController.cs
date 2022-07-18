e05309bc19614535c8e71f404c3c596786815f55
imloki2k <im.loki2k@gmail.com>
2022-07-18 11:21:01 +0700

    upload anh adminproduct

--- LokiPKL/Areas/Admin/Controllers/AdminProductsController.cs
+++ LokiPKL/Areas/Admin/Controllers/AdminProductsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LokiPKL.Models;
+using System.IO;
 
 namespace LokiPKL.Areas.Admin.Controllers
 {
